01e212c801137017d8f3f864d751b3e4b4eb4abe
DietSierraMist <nicholas.kerr@me.com>
2014-03-03 21:31:08 -0700

    This is the first time we are working on the File Copier

---
+++ FileCopier/src/filecopier/FileCopier.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package filecopier;
+
+/**
+ *
+ * @author nicholas
+ */
+public class FileCopier {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This program is going to copy a file!");
+    }
+    
+}
